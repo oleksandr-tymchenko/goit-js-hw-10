8dcd38c48608fd91390602f2f181cf419f9f4eb8
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-04-08 23:35:03 +0300

    completed render of countries arr

--- src/fetch-countries.js
+++ src/fetch-countries.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountries(name) {
-    const url = `${BASE_URL}/name/${name}`;
+    const url = `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`;
     return fetch(url)
         .then((response) => {
             if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -3,17 +3,53 @@ import debounce from 'lodash.debounce';
 
 import './css/styles.css';
 import getRefs from './get-refs';
-import fetchCountries from './fetchCountries';
+import FETCH from './fetch-countries';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 3000;
 
 const refs = getRefs();
-console.log(refs);
+// console.log(refs);
 
-refs.input.addEventListener('input', debounce(onSearchCountry, 300));
+refs.input.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
 
 function onSearchCountry(e) {
-    const name = e.currentTarget.value;
-    console.log(name);
+    const name = e.target.value.trim();
+    
+    FETCH.fetchCountries(name)
+        .then(result => {
+
+            // console.log(result.length)
+            if (result.length >= 2 && result.length <= 10) {
+                renderListOfCountries(result);
+                // console.log(result.Object.values(name))
+            } else if (result.length > 10) {
+                Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+            } else if (result.length = 1) {
+                console.log(result)
+                renderOneCountry(result);
+            }
+    })
+    
+    
     
 };
+
+
+function renderListOfCountries(countries) {
+    const markUp = countries.map(country =>
+        `<li><img src="${country.flags.svg}" alt="A flag" width="30"  />${country.name.official}</li>`).join('');
+    console.log(markUp);
+    refs.countryList.innerHTML = markUp;
+};
+
+function renderOneCountry(country) {
+    console.log(country)
+    // const markUp =
+    //     `<h1>${country.name}</h1>
+    //   <ul>
+    //     <li><span class="item-title">Capital:</span>${country.capital}</li>
+    //     <li><span class="item-title">Population:</span>${country.population}</li>
+    //     <li><span class="item-title">Laguages:</span>${country.languages}</li>
+    //   </ul>`
+    // refs.countryInfo.innerHTML = markUp;
+}
